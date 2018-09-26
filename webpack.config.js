47b983ed37ee8af0e149da78d2097d6014057a0c
panju <panju@shein.com>
2018-09-26 13:08:30 +0800

    xiugai peizhi wenjian

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -10,25 +10,54 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     fileName: '[name].[chunkhash].js'
   },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx']
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         use: {
-          loader: 'babel-loader',
+          use: [
+            'babel-loader',
+            'cache-loade',
+          ],
           options: {
             "presets": ['es2015', 'react'],
+            "plugins": ["transform-runtime"],
           },
-          exclude: /node_modules/,
-        }
+        },
+        exclude: /node_modules/,
+        include: path.resolve('src'),
       },
       {
         test: /\.css/,
-        use: ['style-loader', 'css-loader'],
-        options: {
-          sourceMap: true,
-        },
-      }
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { importLoader: 1 }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            }
+          }
+        ]
+      },
     ]
   },
+  plugins: []
 }
